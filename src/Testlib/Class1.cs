ff78d20ad7d2f970750363505275aaa54ae18748
Florian2501 <florischierz@gmail.com>
2021-06-15 14:08:03 +0200

    Improve documentation of the Testlib

--- src/Testlib/Class1.cs
+++ src/Testlib/Class1.cs
@@ -2,9 +2,25 @@
 
 namespace Testlib
 {
+    /// <summary>
+    ///This is the only class of my Testlib. It devides a method to multply and to add integers.
+    /// </summary>
     public class Class1
     {
+        /// <summary>
+        ///This Method multplies two integer values.
+        /// </summary>
+        /// <returns>
+        /// The product of the two input integers.
+        /// </returns>
         public static int multiply(int a, int b) => a*b;
+
+        /// <summary>
+        ///This Method adds two integer values.
+        /// </summary>
+        /// <returns>
+        /// The sum of the two input integers.
+        /// </returns>
         public static int add (int a, int b)=>a+b;
     }
 }
